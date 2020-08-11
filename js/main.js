7ad5bf2986c1443fc78135de157389e5871991be
Carol <carolbrandes23@gmail.com>
2020-08-11 20:29:33 -0300

    implementação de animação das sections ao serem exibidas (conforme o usuario for dando scroll)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 initTabNav();
 initAccordion();
 initScrollSuave();
-
+initAnimacaoScroll();
 
 
 // para fazer a navegacao entre a img dos animais e o texto referente a cada animal
@@ -80,6 +80,31 @@ function initScrollSuave() {
         // });
 
     }
+}
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.5;
+
+        // é bom já inicializar animacao antes de esperar o usuario dar o scroll na pagina.
+        animarScroll();
+        window.addEventListener('scroll', animarScroll);
+
+        function animarScroll() {
+            sections.forEach(section => {
+                // o getBoundingClientRect() retorna o top, bottom, right, left e outras propriedades do elemento em relação ao window
+                const sectionTop = section.getBoundingClientRect().top - windowMetade;
+
+                if (sectionTop < 0) {
+                    section.classList.add('ativo');
+                }
+
+            })
+        }
+    }
+
 }
 
 
