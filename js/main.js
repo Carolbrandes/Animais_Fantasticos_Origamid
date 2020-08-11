a508990800f6a03be848a379bd127432e28385d6
Carol <carolbrandes23@gmail.com>
2020-08-11 18:43:11 -0300

    implementação do accordion na section faq

--- js/main.js
+++ js/main.js
@@ -1,30 +1,53 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+initTabNav();
+initAccordion();
 
-// só será executado se os elementos existirem na pagina
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
 
-    function activeTab(index) {
-        removeActiveTab(tabContent);
-        tabContent[index].classList.add('ativo');
-    }
 
-    function removeActiveTab(element) {
-        element.forEach(element => element.classList.remove('ativo'));
-    }
+// para fazer a navegacao entre a img dos animais e o texto referente a cada animal
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-            activeTab(index);
+    // só será executado se os elementos existirem na pagina
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
+
+        function activeTab(index) {
+            removeActiveTab(tabContent);
+            tabContent[index].classList.add('ativo');
+        }
+
+        function removeActiveTab(element) {
+            element.forEach(element => element.classList.remove('ativo'));
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-    });
-    // OBS: Do jeito abaixo não iria funcionar, não podemos passar no addEventListener a função sendo executada, colocando os parentesis, dessa forma -> activeTab(index)
-    // tabMenu.forEach((itemMenu, index) => {
-    //     itemMenu.addEventListener('click', activeTab(index))
-    // });
+        // OBS: Do jeito abaixo não iria funcionar, não podemos passar no addEventListener a função sendo executada, colocando os parentesis, dessa forma -> activeTab(index)
+        // tabMenu.forEach((itemMenu, index) => {
+        //     itemMenu.addEventListener('click', activeTab(index))
+        // });
+    }
 }
 
 
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const ativo = 'ativo';
 
+    if (accordionList.length) {
+        accordionList[0].classList.toggle(ativo);
+        accordionList[0].nextElementSibling.classList.toggle(ativo);
+        accordionList.forEach(item => {
+            item.addEventListener('click', activeAccordion);
+        });
 
+        function activeAccordion() {
+            this.classList.toggle(ativo);
+            this.nextElementSibling.classList.toggle(ativo);
+        }
+    }
+}
