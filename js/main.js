a9bbd17df15777235fc865ca31b5a53219fad3f3
Carol <carolbrandes23@gmail.com>
2020-08-11 20:04:41 -0300

    implementação do scroll suave na navegação interna do site

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 initTabNav();
 initAccordion();
+initScrollSuave();
 
 
 
@@ -33,7 +34,7 @@ function initTabNav() {
     }
 }
 
-
+// accordion para section faq
 function initAccordion() {
     const accordionList = document.querySelectorAll('.js-accordion dt');
     const ativo = 'ativo';
@@ -51,3 +52,34 @@ function initAccordion() {
         }
     }
 }
+
+// navegacao do menu
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    linksInternos.forEach(link => link.addEventListener('click', scrollToSection));
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        // se colocarmos event.currentTarget.href vai vir a url completa, já com getAttribute vai vir só o #...
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+
+        // o scrollIntoView já traz a posicao do elemento
+        // o scrollIntoView so funciona no chrome e no firefox
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start' /* alinhe o bloco ao inicio */
+        });
+
+        // forma alternativa  - para pegarmos onde começa cada sessao usamos o offsetTop
+        // const sectionTop = section.offsetTop;
+        // o window.scrollTo recebendo dois params, o quanto queremos mover para o eixo e para o eixo y.
+        // window.scrollTo({
+        //     top: sectionTop,
+        //     behavior: 'smooth'
+        // });
+
+    }
+}
+
+
