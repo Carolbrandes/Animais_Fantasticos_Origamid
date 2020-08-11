1f0ee2eb3e4de7689979f3386852f00175f9a4c3
Carol <carolbrandes23@gmail.com>
2020-08-10 22:28:39 -0300

    navegação por tabs da lista das fotos dos animais com os textos sobre cada um deles

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+// só será executado se os elementos existirem na pagina
+if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index) {
+        removeActiveTab(tabContent);
+        tabContent[index].classList.add('ativo');
+    }
+
+    function removeActiveTab(element) {
+        element.forEach(element => element.classList.remove('ativo'));
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+        itemMenu.addEventListener('click', () => {
+            activeTab(index);
+        });
+    });
+    // OBS: Do jeito abaixo não iria funcionar, não podemos passar no addEventListener a função sendo executada, colocando os parentesis, dessa forma -> activeTab(index)
+    // tabMenu.forEach((itemMenu, index) => {
+    //     itemMenu.addEventListener('click', activeTab(index))
+    // });
+}
+
+
+
+
