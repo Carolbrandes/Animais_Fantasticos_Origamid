07547eaa9fbd96080ffcda0d8e0f92a200054ab1
Carol <carolbrandes23@gmail.com>
2020-08-14 23:37:03 -0300

    implementação da secao que mostra o numero de cada especie de animal

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -2,23 +2,27 @@ export default function initAnimacaoScroll() {
     const sections = document.querySelectorAll('[data-tab="scroll"]');
 
     if (sections.length) {
-        const windowMetade = window.innerHeight * 0.5;
-
-        // é bom já inicializar animacao antes de esperar o usuario dar o scroll na pagina.
-        animarScroll();
-        window.addEventListener('scroll', animarScroll);
+        const windowMetade = window.innerHeight * 0.6;
 
         function animarScroll() {
             sections.forEach(section => {
                 // o getBoundingClientRect() retorna o top, bottom, right, left e outras propriedades do elemento em relação ao window
-                const sectionTop = section.getBoundingClientRect().top - windowMetade;
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
 
-                if (sectionTop < 0) {
+                if (isSectionVisible) {
                     section.classList.add('ativo');
+                }else if(section.classList.contains('ativo')){
+                    section.classList.remove('ativo');
                 }
 
             })
         }
+
+         // é bom já inicializar animacao antes de esperar o usuario dar o scroll na pagina.
+         animarScroll();
+         window.addEventListener('scroll', animarScroll);
+ 
     }
 
 }
